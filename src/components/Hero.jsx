0ff2417936d10be2a86c1acd507fcbdf935c0246
jandsonanjos <jandsonanjos@hotmail.com>
2026-03-13 14:11:09 -0300

    feat: add alteração no profile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,24 +13,6 @@ export default function Hero() {
       <p className="max-w-xl mx-auto text-gray-500">
         Building scalable infrastructure, CI/CD pipelines and modern cloud applications.
       </p>
-
-      <div className="mt-8 flex justify-center gap-4">
-        <a href="https://github.com/Jandsonn" className="bg-blue-600 px-6 py-3 rounded-xl hover:bg-blue-700 text-white">
-          View Projects
-        </a>
-
-        <a 
-            href="https://wa.me/5583998578951?text=Ol%C3%A1%20Jandson%20queremos%20contrat%C3%A1-lo" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="border px-6 py-3 rounded-xl hover:bg-gray-800 hover:border-gray-700 transition-all duration-200">
-            Contact
-        </a>
-
-        <a href="https://www.linkedin.com/in/jandsondosanjos/" className="border px-6 py-3 rounded-xl hover:bg-gray-800 hover:border-gray-700 transition-all duration-200">
-          Profile
-        </a>
-      </div>
     </section>
   )
 }
