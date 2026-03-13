e3be40c455678c029555c1ed3010e7f66b41e19e
jandsonanjos <jandsonanjos@hotmail.com>
2026-03-13 14:15:12 -0300

    tirando espaço

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,9 @@ export default function Hero() {
       <h1 className="text-5xl font-bold mb-4">
         Jandson Anjos
       </h1>
-      
-
       <p className="text-xl text-gray-400 mb-6">
         DevOps Engineer | Cloud | Infrastructure as Code
       </p>
-
       <p className="max-w-xl mx-auto text-gray-500">
         Building scalable infrastructure, CI/CD pipelines and modern cloud applications.
       </p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,6 @@
 export default function Projects() {
-
     <div className="bg-red-500 p-10">
-
 </div>
-
   const projects = [
     {
       id: 1,
@@ -16,33 +13,25 @@ export default function Projects() {
       description: "Monitoring system with Kubernetes & Grafana."
     }
   ]
-
   return (
     <section id="projects" className="py-20">
-
       <h2 className="text-4xl font-bold mb-12 text-center">
         Projects
       </h2>
-
       <div className="grid md:grid-cols-3 gap-8">
-
         {projects.map(project => (
           <div
             key={project.id}
-            className="bg-gray-900 p-6 rounded-2xl shadow-xl hover:scale-105 transition"
-          >
+            className="bg-gray-900 p-6 rounded-2xl shadow-xl hover:scale-105 transition">
             <h3 className="text-xl font-bold mb-2">
               {project.title}
             </h3>
-
             <p className="text-gray-400">
               {project.description}
             </p>
           </div>
         ))}
-
       </div>
-
     </section>
   )
 }
